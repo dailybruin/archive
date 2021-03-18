268ec266bb7baf1ea8ffd303a3be1610bc4eb577
lketcham <63743227+lketcham@users.noreply.github.com>
2021-03-17 17:30:39 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1009,7 +1009,8 @@ function loadnewpage() {
       '0B8WE6yj3c61XUVNMTFQ3SllGazA',
       '1IUbV74C31HOZcf9x97bDVce4hnfFKRlg',
       '1ay-ptXcLRx4yVaOH7xOeEkxmO6IVbdn0',
-			'11qpdQ-t8nt_Rtx1t5VwotACKr_l08Xrt'
+	    '11qpdQ-t8nt_Rtx1t5VwotACKr_l08Xrt',
+	    '1hXyfU3sZJfJi3ojFWtMyso2-IZqEgPkz'
     ]
 		// UPDATE: take the last part of the google drive link of the folder and add to the array above
     folder = folderlist[dataid - 116753] //Minimum range for Google Drive is 116,753.
